7c90a0ab60a1511b2503106ab6867e264815aba6
karojs <you@example.com>
2017-01-11 16:19:35 +0200

    2. lesson

--- features/screens/screen_intro.rb
+++ features/screens/screen_intro.rb
@@ -2,10 +2,20 @@ class ScreenIntro < ScreenBase
   attr_accessor :driver
   def initialize(driver)
     @base = element(:id, 'content')
+    @to_application_button = element(:id, 'com.testdevlab.notifyus:id/close_intro')
+    @filter_icon = element(:id, 'com.testdevlab.notifyus:id/intro_footer_image')
+    @preview = element(:class, 'android.widget.RelativeLayout')
+    @intro_header = element(:id, 'com.testdevlab.notifyus:id/intro_text_header')
+    @intro_footer = element(:id, 'com.testdevlab.notifyus:id/intro_text_footer')
     @driver = driver
   end
 
   def visible?
+    @driver.find_element(@to_application_button[:type], @to_application_button[:value])
+    @driver.find_element(@filter_icon[:type], @filter_icon[:value])
+    @driver.find_element(@preview[:type], @preview[:value])
+    @driver.find_element(@intro_header[:type], @intro_header[:value])
+    @driver.find_element(@intro_footer[:type], @intro_footer[:value])
     @driver.find_element(@base[:type], @base[:value])
   end
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -18,7 +18,7 @@ end
 options = {
   'port' => 5500,
   'portboot' => 5051,
-  'sn' => '00bbb8a0ec4f9f25',
+  'sn' => '090c25240222810f',
   'app' => 'app.apk',
   'appPackage' => 'com.testdevlab.notifyus'
 
