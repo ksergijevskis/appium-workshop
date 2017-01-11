654ec7db5ef24c99600a6aace2352dcfc613da24
karojs <you@example.com>
2017-01-11 16:55:32 +0200

    Add comments regarding choice of locator

--- features/screens/screen_intro.rb
+++ features/screens/screen_intro.rb
@@ -2,10 +2,15 @@ class ScreenIntro < ScreenBase
   attr_accessor :driver
   def initialize(driver)
     @base = element(:id, 'content')
+    #close intro button - single element, id defined
     @to_application_button = element(:id, 'com.testdevlab.notifyus:id/close_intro')
+    #only 1 footer image - id defined
     @filter_icon = element(:id, 'com.testdevlab.notifyus:id/intro_footer_image')
+    #the whole preview section - class defined
     @preview = element(:class, 'android.widget.RelativeLayout')
+    #only 1 intro header - id defined
     @intro_header = element(:id, 'com.testdevlab.notifyus:id/intro_text_header')
+    #only 1 intro footer - id defined
     @intro_footer = element(:id, 'com.testdevlab.notifyus:id/intro_text_footer')
     @driver = driver
   end
